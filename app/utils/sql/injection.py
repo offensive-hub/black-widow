e2ac8251b9697789bda89a9331a1e0e67319e1a3
Fabrizio <webmaster.developer@outlook.com>
2019-05-14 16:21:49 +0200

    fixed comments

--- app/utils/sql/injection.py
+++ app/utils/sql/injection.py
@@ -4,16 +4,17 @@ from app.utils.helpers.logger import Log
 
 
 # Cerca un form all'interno della pagina ritornata dall'url passato come parametro
-# @param url str L'url che restituisce l'html di cui l'injection sui form trovati
-# @param html str L'html di cui l'injection sui form trovati
+# @param url str L'url che restituisce l'html in cui trovare form e tentare
+#        l'injection
+# @param html str L'html contenente i form con cui tentare l'injection sui form trovati
 # @return
 def inject_form(url=None, html=None):
     Log.error('NOT IMPLEMENTED: inject_form('+str(url)+', '+str(html)+')')
 
 
 # Cerca un form all'interno della pagina ritornata dall'url passato come parametro
-# Se non lo trova, cerca un form in tutti i link con lo stesso dominio, presenti
-# nella pagina ritornata dall'url
+# Se non lo trova, o non riesce a fare l'injection, cerca un form in tutti i link
+# con lo stesso dominio, presenti nella pagina ritornata dall'url
 # @param url str L'url che restituisce l'html di cui l'injection sui form trovati
 # @return
 def deep_inject_form(url):
