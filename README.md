c953ab65803624d3c21ec03693476b3add22d41d
Fabrizio <fabri.fubels@gmail.com>
2019-06-22 13:38:58 +0200

    customizing argparse

--- README.md
+++ README.md
@@ -4,10 +4,11 @@
 ## CTF Tool (Language: [python3.7](https://www.python.org/downloads/))
 
 #### Dipendenze:
- - [pyshark](https://pypi.org/project/pyshark/): `pip3 install pyshark`
- - [PyQt5](https://pypi.org/project/PyQt5/): `pip3 install PyQt5`
- - [PyTidyLib](https://pypi.org/project/pytidylib/): `pip3 install pytidylib`
- - [Sqlmap](https://pypi.org/project/sqlmap/): `sudo apt-get install sqlmap`
+ - [pyshark](https://pypi.org/project/pyshark/): `sudo pip3 install pyshark`
+ - [PyQt5](https://pypi.org/project/PyQt5/): `sudo pip3 install PyQt5`
+ - [PyQtWebEngine](https://pypi.org/project/PyQtWebEngine/): `sudo pip3 install PyQtWebEngine`
+ - [PyTidyLib](https://pypi.org/project/pytidylib/): `sudo pip3 install pytidylib`
+ - [Sqlmap](https://pypi.org/project/sqlmap/): `sudo pip3 install sqlmap`
 
 #### Funzionalità:
  - Sniffing (anche con regex sul contenuto delle richieste/risposte) per rubare flag catturate da altri;
--- black-widow.py
+++ black-widow.py
@@ -1,45 +1,49 @@
 #!/usr/bin/env python3
 
-import os, sys, getopt, argparse, app
+import os, sys, getopt, lib, app
 
 VERSION = '1.0.0#alpha'
+
 
 class AppType:
     CMD=''
     GUI='GUI'
 
-class CapitalisedHelpFormatter(argparse.HelpFormatter):
+
+class CapitalisedHelpFormatter(lib.args.argparse.HelpFormatter):
     def add_usage(self, usage, actions, groups, prefix=None):
         if prefix is None: prefix = 'Usage: '
         return super(CapitalisedHelpFormatter, self).add_usage(
         usage, actions, groups, prefix)
 
-def usage():
-    #--- Custom print_usage ---#
-    def print_usage(output): return
-
+# Creates the argument parser and return the parsed input arguments
+def get_arguments():
     #--- Header image ---#
     def print_header():
         header_ascii = app.utils.helpers.storage.read_file(app.env.RES_PATH+'/' + str(app.env.APP_PROC) + '-ascii.txt')
         header_ascii = header_ascii.replace('{version}', VERSION)
         print('\n' + header_ascii + '\n')
 
-    #--- Usage ---#
-    parser = argparse.ArgumentParser(add_help=False, usage=sys.argv[0]+' [Options]', formatter_class=CapitalisedHelpFormatter)
-    parser.print_usage = print_usage   # Already printed with print_help
+    #--- Parser ---#
+    parser = lib.args.Arguments()
+    print(dir(parser))
+    exit(0)
 
     #--- Options ---#
-    options = parser.add_argument_group("Options")
+    options = parser.add_argument_group("General Options")
     options.add_argument("-h", "--help", help="Show this help message and exit",
                          action="store_true")
-    #options.add_argument("-v", "--verbose", help="Increase output verbosity",
-    #                     action="store_true")
     options.add_argument("-v", "--version", help="Show program's version number and exit",
                          action="store_true")
 
+    options_pcap = parser.add_argument_group("Sniffing Options")
+    options_pcap.add_argument("--sniff", help="Sniff Packages", type=str, metavar='FILTERS')
 
-    options_pcap = options.add_argument_group("Sniffing")
-    options_pcap.add_argument("--sniff", help="Sniff Packages", type=str, metavar='[FILTERS]')
+    options_sql = parser.add_argument_group("SQL Options")
+    options_sql.add_argument("--sql", help="Try injection in a website. Avilable settings: url=https://example.com deep=true", type=str, metavar='[SETTINGS]')
+
+    print(dir(options))
+    exit(0)
 
     try:
         args = parser.parse_args()
@@ -56,23 +60,24 @@ def usage():
 
     print_header()
 
-    if (args.help):
+    # Check if at least one argument is set
+    arg_set = False
+    for arg in args._get_kwargs(): arg_set = arg[1] or arg_set
+    if (args.help or not arg_set):
+        # Print help
         parser.print_help()
         print()
         parser.exit(0)
 
-    # TODO: ask interface, [dest_file], [filter]
-    if (args.sniff):
-        import pprint
-        def pcap_callback(pkt_dict):
-            pprint.pprint(pkt_dict)
-    #app.utils.sniffing.sniff_pcap(src_file=None, interface=interface, dest_file=test_pcap_out, filter=filter7, limit_length=10000, callback=pcap_callback)
+    global usage
+    usage = parser.print_help
 
-    print()
+    return args
 
 # Startup
 def boot(app_type):
     app.utils.helpers.logger.Log.info(app.env.APP_NAME+' '+str(app_type)+' GUI started, PID='+str(os.getpid()))
+
 
 # Main function for GUI app
 def main_gui():
@@ -87,35 +92,14 @@ def main_cmd():
 
 # Main function generic app
 def main():
-    usage()
+    arguments = get_arguments()
+    # TODO: ask interface, [dest_file], [filter]
+    if (arguments.sniff):
+        import pprint
+        def pcap_callback(pkt_dict):
+            pprint.pprint(pkt_dict)
+    #app.utils.sniffing.sniff_pcap(src_file=None, interface=interface, dest_file=test_pcap_out, filter=filter7, limit_length=10000, callback=pcap_callback)
     return()
-    try:
-        args = sys.argv[1:]
-        opts, args = getopt.getopt(args, "hi:o:", ["ifile=", "ofile="])
-        print('opts:')
-        print(opts)
-        print('args:')
-        print(args)
-        print('---------')
-    except getopt.GetoptError:
-        usage()
-        sys.exit(2)
-    return
-    inputfile = ''
-    outputfile = ''
-    print(opts)
-    for opt, arg in opts:
-        print(opt)
-        if opt == '-h':
-            usage()
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
-    print ('Input file is ' + inputfile)
-    print ('Output file is ' + outputfile)
-
 
 if __name__ == "__main__":
     main()
