0cf5560caa93d21ca764f62a13920c938b2ebd56
Fabrizio <fabri.fubels@gmail.com>
2019-06-29 18:22:58 +0200

    cleaned up code

--- app/env.py
+++ app/env.py
@@ -1,16 +1,16 @@
 from os.path import dirname, realpath
 
 # Variabili d'ambiente
-APP_DEBUG=True
-APP_NAME='Black Widow'
-APP_PROC='black-widow'
-APP_PATH=dirname(realpath(__file__))                # /path/to/app
-APP_STORAGE=APP_PATH+'/storage'
-APP_STORAGE_OUT=APP_STORAGE+'/out'
-APP_SETTINGS=APP_STORAGE+'/settings.json'
-APP_TMP='/tmp/black-widow'
-APP_LOGFILE=APP_TMP+'/black-widow.log'
+APP_DEBUG = True
+APP_NAME = 'Black Widow'
+APP_PROC = 'black-widow'
+APP_PATH = dirname(realpath(__file__))  # /path/to/app
+APP_STORAGE = APP_PATH + '/storage'
+APP_STORAGE_OUT = APP_STORAGE + '/out'
+APP_SETTINGS = APP_STORAGE + '/settings.json'
+APP_TMP = '/tmp/black-widow'
+APP_LOGFILE = APP_TMP + '/black-widow.log'
 
-RES_PATH=APP_PATH=dirname(APP_PATH)+'/resources'
+RES_PATH = APP_PATH = dirname(APP_PATH) + '/resources'
 
-FLAG_REGEX='[A-Z0-9]{31}='                          # Default flag regex
+FLAG_REGEX = '[A-Z0-9]{31}='  # Default flag regex
