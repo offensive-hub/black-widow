173c45de0020d9191ab344977939797ae83fc86c
Fabrizio <fabri.fubels@gmail.com>
2019-06-29 19:03:48 +0200

    cleaned up code

--- app/utils/helpers/util.py
+++ app/utils/helpers/util.py
@@ -2,14 +2,18 @@
 Funzioni generiche, utili in tutte le parti del software.
 """
 
-import json, re
-from app.utils.helpers import storage
+import json
+import re
+
 from app.env import APP_DEBUG
+from app.utils.helpers import storage
 from app.utils.helpers.logger import Log
+
 
 # @return dict Il json nel file in formato dict
 def get_json(file):
     return get_json_str(storage.read_file(file))
+
 
 # @return dict Il json in formato dict
 def get_json_str(string):
@@ -18,33 +22,38 @@ def get_json_str(string):
     except json.decoder.JSONDecodeError:
         return dict()
 
+
 # @param dictionary dict Il dizionario da scrivere nel file in formato json
 def set_json(dictionary, file):
     return storage.overwrite_file(json.dumps(dictionary), file)
 
+
 # @return True se string contiene regex
 def regex_in_string(regex, string):
-    if (APP_DEBUG): Log.info('CALLED: regex_in_string('+str(regex)+', '+str(string)+')')
+    if APP_DEBUG: Log.info('CALLED: regex_in_string(' + str(regex) + ', ' + str(string) + ')')
     reg = re.compile(regex)
     matches = re.findall(reg, string)
     return len(matches) > 0
+
 
 # @param regex la regex da trovare in string
 # @param replace la stringa con cui sostituire la regex
 # @param string la stringa in cui trovare la regex
 # @return la string passata per argomento (find), con la sostituzione
 def replace_regex(regex, replace, string):
-    return re.sub(regex, replace, string, flags = re.M)
+    return re.sub(regex, replace, string, flags=re.M)
+
 
 # @param element Un oggetto
 # @return True se element è un elemento listabile, False altrimenti
 def is_listable(object):
     return type(object) in (list, tuple, dict, range)
 
+
 # Fa eseguire al sistema operativo i comandi in args
 # @param *args "cmd [argomenti]"        // ES: "netstat -tuan"
 def pexec(*args):
-    if (APP_DEBUG): Log.info('CALLED: pexec'+str(args))
+    if (APP_DEBUG): Log.info('CALLED: pexec' + str(args))
     """
     try:
         p=subprocess.Popen(args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
