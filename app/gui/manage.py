3f2f02b6203d8f6d03ccbbb9393847756d445fc2
Fabrizio Fubelli <fabrizio.fubelli@mashfrog.com>
2019-07-26 14:15:40 +0200

    Integrating Gunicorn

--- app/gui/manage.py
+++ app/gui/manage.py
@@ -1,47 +1,32 @@
 import os
-import webbrowser
+import sys
 
-# from django import setup as django_setup
+from gunicorn.app.wsgiapp import run
 from django.core import management
-from django.core.servers.basehttp import simple_server
 
 from app.env_local import APP_WEB_HOST, APP_WEB_PORT
-from app.env import EXEC_PATH
+from app.env import EXEC_PATH, APP_NAME
 from app.utils.helpers.logger import Log
 from app.utils.helpers.network import get_ip_address
-from app.utils.helpers.multitask import multithread, multiprocess
 from app.gui.web.wsgi import WEB_PACKAGE
-from app.gui.web.wsgi import application
 
 
-def _get_preferred_ip():
+def _get_bind_socket():
     host = APP_WEB_HOST
     if host is None:
         host = get_ip_address()
     if host is None:
         host = '0.0.0.0'
-    return host
+    return str(host) + ':' + str(APP_WEB_PORT)
 
 
 def django_gui():
-    host = _get_preferred_ip()
-
-    def _run_django():
-        # django_setup()
-        django_server = simple_server.make_server(host, APP_WEB_PORT, application)
-        Log.success("Django web-server started!")
-        django_server.serve_forever()
-        # management.call_command('runserver', '--noreload')
-
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", WEB_PACKAGE + ".settings")
-
-    Log.info("Starting the django web-server in a parallel thread")
-    multiprocess(_run_django, (), True, 1)
-
-    url = "http://" + str(host) + ':' + str(APP_WEB_PORT)
-    Log.info("Visit: " + url)
-
-    webbrowser.open(url, new=2)
+    # Go to "/app/gui/" directory
+    sys.path.insert(0, os.path.dirname(__file__))
+    bind_host = _get_bind_socket()
+    Log.info("Starting " + str(APP_NAME) + ' GUI')
+    sys.argv = [sys.argv[0], 'web.wsgi', '-b', bind_host]
+    run()
 
 
 def django_cmd(args):
--- app/gui/web/wsgi.py
+++ app/gui/web/wsgi.py
@@ -8,15 +8,15 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-# import sys
+import sys
 
-# from os.path import dirname
+from os.path import dirname
 from django.core.wsgi import get_wsgi_application
 
 WEB_PACKAGE = 'app.gui.web'
 
-# root_path = dirname(dirname(dirname(dirname(__file__))))    # to access at "app" package
-# sys.path.insert(0, root_path)
+root_path = dirname(dirname(dirname(dirname(__file__))))    # to access at "app" package
+sys.path.insert(0, root_path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", WEB_PACKAGE + ".settings")
 
