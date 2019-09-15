e8b13ebfaa6e71531b10aed0325681e1aa0f808c
Fabrizio <fabri.fubels@gmail.com>
2019-09-16 00:27:10 +0200

    Added license terms

--- app/utils/sniffing/pcap.py
+++ app/utils/sniffing/pcap.py
@@ -1,7 +1,36 @@
 # -*- coding: utf-8 -*-
 
 """
-Packet Capture (pcap)
+*********************************************************************************
+*                                                                               *
+* pcap.py -- Packet Capture (pcap)                                              *
+*                                                                               *
+* Methods to sniff the network traffic through pyshark.                         *
+*                                                                               *
+* pyshark repository:                                                           *
+* https://github.com/KimiNewt/pyshark                                           *
+*                                                                               *
+* pyshark license:                                                              *
+* https://raw.githubusercontent.com/KimiNewt/pyshark/master/LICENSE.txt         *
+*                                                                               *
+********************** IMPORTANT BLACK-WIDOW LICENSE TERMS **********************
+*                                                                               *
+* This file is part of black-widow.                                             *
+*                                                                               *
+* black-widow is free software: you can redistribute it and/or modify           *
+* it under the terms of the GNU General Public License as published by          *
+* the Free Software Foundation, either version 3 of the License, or             *
+* (at your option) any later version.                                           *
+*                                                                               *
+* black-widow is distributed in the hope that it will be useful,                *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of                *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 *
+* GNU General Public License for more details.                                  *
+*                                                                               *
+* You should have received a copy of the GNU General Public License             *
+* along with black-widow.  If not, see <http://www.gnu.org/licenses/>.          *
+*                                                                               *
+*********************************************************************************
 """
 
 import codecs  # gzip
--- app/utils/sql/sqlmapcli.py
+++ app/utils/sql/sqlmapcli.py
@@ -1,3 +1,36 @@
+"""
+*********************************************************************************
+*                                                                               *
+* sqlmapcli.py -- sqlmap client.                                                *
+*                                                                               *
+* Class to interface with the sqlmap local server.                              *
+*                                                                               *
+* sqlmap repository:                                                            *
+* https://github.com/sqlmapproject/sqlmap                                       *
+*                                                                               *
+* sqlmap license:                                                               *
+* https://raw.githubusercontent.com/sqlmapproject/sqlmap/master/LICENSE         *
+*                                                                               *
+********************** IMPORTANT BLACK-WIDOW LICENSE TERMS **********************
+*                                                                               *
+* This file is part of black-widow.                                             *
+*                                                                               *
+* black-widow is free software: you can redistribute it and/or modify           *
+* it under the terms of the GNU General Public License as published by          *
+* the Free Software Foundation, either version 3 of the License, or             *
+* (at your option) any later version.                                           *
+*                                                                               *
+* black-widow is distributed in the hope that it will be useful,                *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of                *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 *
+* GNU General Public License for more details.                                  *
+*                                                                               *
+* You should have received a copy of the GNU General Public License             *
+* along with black-widow.  If not, see <http://www.gnu.org/licenses/>.          *
+*                                                                               *
+*********************************************************************************
+"""
+
 from pprint import pprint
 from sqlmap.lib.utils.api import server as sqlmap_server
 
