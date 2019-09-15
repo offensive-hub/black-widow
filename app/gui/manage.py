4e02779c04f1ebafe6bccbfe135caad6fa992c64
Fabrizio <fabri.fubels@gmail.com>
2019-09-15 23:23:35 +0200

    added LICENSE TERMS in app.gui.manage

--- app/gui/manage.py
+++ app/gui/manage.py
@@ -11,6 +11,32 @@ from app.utils.helpers.network import get_ip_address
 from app.gui.web.wsgi import WEB_PACKAGE
 
 
+""" ********************************************************************* """
+"""                                                                       """
+""" manage.py -- Django manager: this file provide methods to use the     """
+""" Django manager to test the web server                                 """
+"""                                                                       """
+"""                                                                       """
+""" **************** IMPORTANT BLACK-WIDOW LICENSE TERMS **************** """
+"""                                                                       """
+""" This file is part of Black-Widow.                                     """
+"""                                                                       """
+""" Black-Widow is free software: you can redistribute it and/or modify   """
+""" it under the terms of the GNU General Public License as published by  """
+""" the Free Software Foundation, either version 3 of the License, or     """
+""" (at your option) any later version.                                   """
+"""                                                                       """
+""" Black-Widow is distributed in the hope that it will be useful,        """
+""" but WITHOUT ANY WARRANTY; without even the implied warranty of        """
+""" MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         """
+""" GNU General Public License for more details.                          """
+"""                                                                       """
+""" You should have received a copy of the GNU General Public License     """
+""" along with Black-Widow.  If not, see <http://www.gnu.org/licenses/>.  """
+"""                                                                       """
+""" ********************************************************************* """
+
+
 def _get_bind_socket():
     host = APP_WEB_HOST
     if host is None:
