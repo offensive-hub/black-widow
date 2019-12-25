b428c1e4fb79bf24ea2bb79261803e5b8f103148
Fabrizio <fabri.fubels@gmail.com>
2019-12-25 17:44:47 +0100

    mac lookup ok

--- app/utils/sniffing/pcap.py
+++ app/utils/sniffing/pcap.py
@@ -35,12 +35,102 @@
 
 import codecs
 import socket
+from math import floor
+
 import numpy
 import pyshark
 
 from app.utils import settings
 from app.utils.helpers.util import replace_regex, regex_is_string
 from app.utils.helpers.logger import Log
+from app.utils.requests import request
+
+
+class MacManufacturerLookup:
+    """
+    The Mac Manufacturer Lookup class
+    _________________________
+    | 1 | 2 | 3 | 4 | 5 | 6 |  =  48bit (6 * 8bit)
+    {^^^^OUI^^^}^{^^^NIC^^^^}
+
+    MAC Address: 6 Octets -> First 3 Octets:  OUI "Organisationally Unique Identifier"
+                                   |--- 1st Octet: 7th bit (1=locally administered, 0=globally unique)
+                                                   8th bit (0=unicast, 1=multicast)
+                             Second 3 Octets: NIC "Network Interface Controller"
+    """
+
+    MANUFACTURERS_URL = 'https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf'
+    MANUFACTURERS_DETAIL_DICT = {
+        0: 'mac',
+        1: 'company',
+        2: 'device'
+    }
+
+    def __init__(self):
+        self.manufacturer_dict = dict()
+        manufacturer_response = request(MacManufacturerLookup.MANUFACTURERS_URL)
+        manufacturer_list = manufacturer_response.text.splitlines()
+        for manufacturer in manufacturer_list:
+            if len(manufacturer) < 1:
+                continue
+            if manufacturer[0] == '#':
+                continue
+            manufacturer_details = manufacturer.split('\t')
+            i = 0
+            mac = None
+            company = None
+            device = None
+            comment = None
+            for detail in manufacturer_details:
+                if detail == '':
+                    continue
+                if i == 0:
+                    # MAC address
+                    mac_detail = detail.split('/')
+                    if len(mac_detail) == 2:
+                        # The mac has a sub mask, so the dict key is the first n bits
+                        sub_mask = int(mac_detail[1]) / 4
+                        mac_sub_mask = floor(sub_mask + (sub_mask / 2))
+                        mac = mac_detail[0][0:mac_sub_mask]
+                    elif len(mac_detail) == 1:
+                        # The mac has not a sub mask
+                        mac = mac_detail[0]
+                    else:
+                        Log.error("Wrong mac address: " + str(detail))
+                        break
+                elif i == 1:
+                    # Company
+                    company = detail
+                elif i == 2:
+                    # Device
+                    device = detail
+                elif i == 3:
+                    # Comment
+                    comment = detail
+                else:
+                    Log.error("Wrong manufacturer details: " + str(manufacturer_details))
+                    break
+                i += 1
+            if mac is None:
+                Log.error("Wrong manufacturer details: " + str(manufacturer_details))
+                continue
+            self.manufacturer_dict[mac] = {
+                'company': company,
+                'device': device,
+                'comment': comment
+            }
+
+    def lookup(self, mac: str) -> dict or None:
+        """
+        Lookup the mac address
+        :param mac: The mac address to lookup
+        :rtype: dict or None
+        """
+        for i in range(len(mac), 0, -1):
+            manufacturer = self.manufacturer_dict.get(mac[0:i])
+            if manufacturer is not None:
+                return manufacturer
+        return None
 
 
 def sniff_pcap(filters=None, src_file=None, dest_file=None, interface=None, limit_length=None, callback=None):
@@ -53,6 +143,11 @@ def sniff_pcap(filters=None, src_file=None, dest_file=None, interface=None, limi
     :param limit_length: The limit length of each packet field (they will be truncated), or None
     :param callback: The callback method to call (or None)
     """
+
+    mac_manufacturer = MacManufacturerLookup()
+    manufacturer = mac_manufacturer.lookup('00:1B:C5:00:00:00')
+    print(manufacturer)
+    exit(0)
 
     def __is_hash__(text: str) -> bool:
         return len(text) >= 8 and regex_is_string('^(([A-Z]|[a-z]|[0-9])+)+$', text)
@@ -70,8 +165,11 @@ def sniff_pcap(filters=None, src_file=None, dest_file=None, interface=None, limi
         # Log.info('Analyzing packet number ' + str(pkt.number))
         # Log.info('Layers: ' + str(pkt.layers))
         layers_dict = dict()
-        # pkt.pretty_print() # Printa il pacchetto in modo comprensibile (non mostra importanti campi e non decodifica)
+        pkt.pretty_print()  # Printa il pacchetto in modo comprensibile (non mostra importanti campi e non decodifica)
         for layer in pkt.layers:
+            # for field_line in layer._get_all_field_lines():
+            #     print(field_line)
+
             layer_fields = {}
 
             # Ripasso Layers:
@@ -96,11 +194,11 @@ def sniff_pcap(filters=None, src_file=None, dest_file=None, interface=None, limi
                         # Decodifico in utf-8 (il doc non era in esadecimale)
                         field = codecs.decode(bytes(dirty_field, encoding='utf-8')).replace("\\r\\n", "")
                     # Ordino codice sostituendo caratteri di accapo e di tabulazione e pulisco la stringa
-                    field = field\
-                        .replace('\\xa', '\n')\
-                        .replace('\\xd', '\n')\
-                        .replace('\\x9', '\t')\
-                        .replace('\\n', '\n')\
+                    field = field \
+                        .replace('\\xa', '\n') \
+                        .replace('\\xd', '\n') \
+                        .replace('\\x9', '\t') \
+                        .replace('\\n', '\n') \
                         .strip()
 
                 # salvo campi originale e decodificato
