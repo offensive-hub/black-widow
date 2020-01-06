6cb531858a5f78fe85cee1d2726534fa0711fef5
Fabrizio <fabri.fubels@gmail.com>
2020-01-06 02:20:16 +0100

    adding layers on pcap modal

--- app/gui/web/black_widow/static/js/black-widow.js
+++ app/gui/web/black_widow/static/js/black-widow.js
@@ -202,3 +202,22 @@ const notify = function(msg, type='warning', icon='warning', from='top', align='
         }
     });
 };
+
+const initAccordions = function() {
+    $('.toggle').click(function(e) {
+        e.preventDefault();
+
+        const $this = $(this);
+
+        if ($this.next().hasClass('show')) {
+            $this.next().removeClass('show');
+            $this.next().slideUp(350);
+        } else {
+            $this.parent().parent().find('li .inner').removeClass('show');
+            $this.parent().parent().find('li .inner').slideUp(350);
+            $this.next().toggleClass('show');
+            $this.next().slideToggle(350);
+        }
+    });
+};
+initAccordions();
