fabfa56bcecd3ce742c48c3c76f7eeaf3c0b223e
Fabrizio Fubelli <fabrizio@fubelli.org>
2022-04-29 21:59:03 +0200

    Updated setup

--- docs/packaging/setup.sh
+++ docs/packaging/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $1 = '-t' ];then
+  repo_url='https://test.pypi.org/legacy/'
+else
+  repo_url='https://pypi.org/'
+fi
+
 # Remove old build
 rm -rf ./build/ ./dist/ ./src/black_widow.egg-info/
 
@@ -9,7 +15,7 @@ cp -f ./src/black_widow/README.md ./
 python3 setup.py sdist bdist_wheel
 
 # Upload new build
-python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+python3 -m twine upload --repository-url "$repo_url" dist/*
 
 sudo pip3 uninstall black-widow
 
@@ -17,5 +23,8 @@ echo
 echo "Install the new black-widow version"
 echo
 echo "Eg."
+echo "  test:"
 echo "    sudo pip3 install -i https://test.pypi.org/simple/ black-widow==1.8.0"
+echo "  prod:"
+echo "    sudo pip3 install -i https://pypi.org/simple/ black-widow==1.8.0"
 echo
